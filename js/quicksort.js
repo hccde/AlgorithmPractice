dac4004441de0b1e57436e6d430698bad48a9adb
hccde <1076663958@qq.com>
2016-05-06 23:59:11 +0800

    so sad  晚上光看书了，慢慢来吧，应该先完成算法练习的

---
+++ js/quicksort.js
@@ -0,0 +1,12 @@
+(function(){
+	function quickSork(){
+		var raw =  [].slice.call(arguments);
+		raw.length>1:sort(raw):nil;
+		function sort(arr,len){
+			}
+		}
+		return raw;
+	}
+
+	return quickSork
+})()
