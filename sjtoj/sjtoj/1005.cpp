6847c2cc0e27cc34a00ad977bc45e62192eb7686
hccde <1076663958@qq.com>
2016-08-22 22:51:05 +0800

    1005 all AC 简单就是好 不用提前优化 过度设计

--- sjtoj/sjtoj/1005.cpp
+++ sjtoj/sjtoj/1005.cpp
@@ -6,37 +6,82 @@ using std::endl;
 const int SUM = 45;
 const int MULT = 362880;
 
-int tempSum(int arr[],int i,int j){
-    int tem = 0;
-    int sum = 0;
-    int mult = 1;
-    int result = 1;
-    for(int n=0;n<3;n++){
-        for (int m=0; m<3; m++) {
-            tem = arr[(n+j)*9+i+m];
-            sum = tem+sum;
-            mult = mult*tem;
-        }
+int compare(int num,int numlist[]){
+    int result = 0;
+    for(int i=0;i<9;i++){
+        if(numlist[i] == num){result=1;break;};
     }
-    if(!(sum == SUM &&mult == MULT)){
-        result = 0;
-    }
-    
     return result;
 }
 
-int  tempCheck(int arr[],int resultValue){
+int tempcompare(int arr[],int i,int j){
+    int result = 0;
+    int numlist[9] = {0};
+    for(int n=0;n<3;n++){
+        for (int m=0; m<3; m++) {
+            int res = 0;
+            int tem =0;
+            tem = arr[(n+j)*9+i+m];
+            res = compare(tem, numlist);
+            numlist[n*3+m] = tem;
+            if(res){
+                result = 1;
+                break;
+            }
+        }
+    }
+    return result;
+}
+
+int temcheckEvery(int arr[]){
+    int result = 1;
     for(int i=0;i<7;){
         for (int j=0; j<7;) {
-            resultValue = tempSum(arr, i, j);
-            if(!resultValue){
+            int res = 0;
+            res = tempcompare(arr, i,j);
+            if(res){//res == 1 break
+                result = 0;
                 break;
             }
             j=j+3;
         }
         i =i+3;
     }
-    return resultValue;
+    return result;
+}
+
+int checkEvery(int arr[]){
+    int result = 1;
+    bool switchButton =true;
+    //row
+    for(int i = 0;i< 9 ;i++){
+        int temlist[9]={0};
+        for(int j = 0;j<9;j++){
+            int res=0;
+            res = compare(arr[i*9+j], temlist);
+            temlist[j] = arr[i*9+j];
+            if(res){result=0;switchButton=false;break;}
+        }
+    }
+    
+    //col
+    if(switchButton){
+        for(int i = 0;i< 9 ;i++){
+            int temlist[9]={0};
+            for(int j = 0;j<9;j++){
+                int res;
+                res = compare(arr[j*9+i], temlist);
+                temlist[j] = arr[j*9+i];
+                if(res){result=0;switchButton=false;break;}
+            }
+        }
+    }
+    
+    //tem
+    if (switchButton) {
+        result = temcheckEvery(arr);
+    }
+    return result;
 }
 
 int main(){
@@ -49,46 +94,18 @@ int main(){
         int sudoku[81] ={0};
         bool switchButton = true;
         for(int i = 0;i< 9 ;i++){
-            
-            int rowSum = 0;
-            int rowmult = 1;
             for(int j = 0;j<9;j++){
                 int tem = 0;
                 cin>>tem;
                 if(switchButton){
                     sudoku[j+i*9] = tem;
-                    rowSum = tem+rowSum;
-                    rowmult = tem*rowmult;
                 }
             }
-            //row
-            if(!(rowSum==SUM&&rowmult==MULT)){
-                result[k] = 0;
-                switchButton = false;
-                break;
-            }
         }
+
         
-        //col
-        if(switchButton){
-            for(int n = 0;n<9;n++){
-                int colSum = 0;
-                int colmult = 1;
-                for (int m = 0; m<9; m++) {
-                    int tem = sudoku[m*9+n];
-                    colmult = colmult*tem;
-                    colSum  = colSum + tem;
-                }
-                if(!(colSum == SUM&&colmult == MULT)){
-                    result[k] = 0;
-                    switchButton = false;
-                    break;
-                }
-            }
-        }
-        //temp 3*3
-        if(switchButton){
-            result[k] = tempCheck(sudoku,result[k]);
+        if(1){
+           result[k] =  checkEvery(sudoku);
         }
     }
     
