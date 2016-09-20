f01ddbe43606224ebb6b58a7be90bfb71a94f14c
hccde <1076663958@qq.com>
2016-09-20 14:58:42 +0800

    无论如何记得使用之前一定一定初始化，踩坑

--- uoj/uoj/stringmatch.cpp
+++ uoj/uoj/stringmatch.cpp
@@ -1,118 +1,118 @@
+////
+////  stringmatch.cpp
+////  uoj
+////
+////  Created by admos on 16/9/17.
+////  Copyright © 2016年 admos. All rights reserved.
+////连续匹配的KMP算法
+////对于char字符数组，定义一个数据结构 char[0] = length，最后一位放 '\0'
+////因为是char数组，length在使用的时候注意要转换为int型，使用结构体更恰当，不容易错
 //
-//  stringmatch.cpp
-//  uoj
+//#include <stdio.h>
+//#include <iostream>
+//using std::cin;
+//using std::cout;
+//using std::endl;
 //
-//  Created by admos on 16/9/17.
-//  Copyright © 2016年 admos. All rights reserved.
-//连续匹配的KMP算法
-//对于char字符数组，定义一个数据结构 char[0] = length，最后一位放 '\0'
-//因为是char数组，length在使用的时候注意要转换为int型，使用结构体更恰当，不容易错
-
-#include <stdio.h>
-#include <iostream>
-using std::cin;
-using std::cout;
-using std::endl;
-
-typedef struct charstr {
-    int length;
-    char arr[1000];
-} CHARSTR;
-
-int match(CHARSTR *str,CHARSTR *strchild,int nextarr[]){
-    int count(0);
-    int length = str->length;
-    int child_length = strchild->length;
-    //移动子串
-    int j = 1;
-    for(int i=1;i<=length;){
-        bool ismatch(true);
-        for(;j<=child_length;j++){
-            if(str->arr[i+j-1]!=strchild->arr[j]){//不匹配
-                ismatch = false;
-                int offset = (j-1-nextarr[j-1]);
-                j = j-offset;
-                offset = offset>0?offset:1;
-                i = i+offset;
-                break;
-            }else{
-                //当前位匹配
-            }
-        }
-        //完全匹配
-        if(ismatch){
-            //为下次匹配做准备
-        int offset = (child_length-nextarr[child_length]);
-        j = j -offset;
-        offset = offset>0?offset:1;
-        i = i+offset;
-        count = count+1;
-        }
-    }
-    return count;
-}
-
-int each_next(CHARSTR *str,int cur){//cur == length
-    int common(0);
-    for (int i = (cur-1); i >= 1; i--) {//前缀
-        bool flag(true);
-        for (int j = 1; j<=i; j++) {
-            if(str->arr[j] != str->arr[cur-i+j]){
-                flag = false;
-                common = 0;//不相同
-                break;
-            }
-            common = common+1;//计算共有长度
-        }
-        if(flag){break;}//找到相同的元素,common已经计算出
-    }
-    return common;
-}
-
-void next(CHARSTR *strchild,int nextarr[]){
-    int length = strchild->length;
-    for (int i = 1; i<=length; i++) {
-        nextarr[i] = each_next(strchild,i);
-    }
-}
-
-void input(CHARSTR *str){
-    for(int j = 1;j<1001;j++){
-        char letter('0');
-        scanf("%c",&letter);
-        if(letter == '\n'){
-            str->length = (j-1);
-            break;
-        }
-        str->arr[j] = letter;
-    }
-}
-
-int main(){
-    int n(0);
-    char enter;
-    scanf("%d",&n);
-    scanf("%c",&enter);
-    int* result = new int[n];
-    CHARSTR strchild;
-    CHARSTR str;
-    for (int i = 0; i<n; i++) {
-        input(&strchild);
-        input(&str);
-        int nextarr[11]={0};//与字符串数据结构一致
-        next(&strchild,nextarr);
-        result[i] =match(&str,&strchild,nextarr);
-//        cout<<match(str,strchild,nextarr)<<endl;
-    }
-    
-    for(int i = 0;i<n;i++){
-        if(i == n-1){
-            cout<<result[i];
-        }else{
-            cout<<result[i]<<endl;
-        }
-        
-    }
-    delete []result;
-    return 0;
-}
+//typedef struct charstr {
+//    int length;
+//    char arr[1000];
+//} CHARSTR;
+//
+//int match(CHARSTR *str,CHARSTR *strchild,int nextarr[]){
+//    int count(0);
+//    int length = str->length;
+//    int child_length = strchild->length;
+//    //移动子串
+//    int j = 1;
+//    for(int i=1;i<=length;){
+//        bool ismatch(true);
+//        for(;j<=child_length;j++){
+//            if(str->arr[i+j-1]!=strchild->arr[j]){//不匹配
+//                ismatch = false;
+//                int offset = (j-1-nextarr[j-1]);
+//                j = j-offset;
+//                offset = offset>0?offset:1;
+//                i = i+offset;
+//                break;
+//            }else{
+//                //当前位匹配
+//            }
+//        }
+//        //完全匹配
+//        if(ismatch){
+//            //为下次匹配做准备
+//        int offset = (child_length-nextarr[child_length]);
+//        j = j -offset;
+//        offset = offset>0?offset:1;
+//        i = i+offset;
+//        count = count+1;
+//        }
+//    }
+//    return count;
+//}
+//
+//int each_next(CHARSTR *str,int cur){//cur == length
+//    int common(0);
+//    for (int i = (cur-1); i >= 1; i--) {//前缀
+//        bool flag(true);
+//        for (int j = 1; j<=i; j++) {
+//            if(str->arr[j] != str->arr[cur-i+j]){
+//                flag = false;
+//                common = 0;//不相同
+//                break;
+//            }
+//            common = common+1;//计算共有长度
+//        }
+//        if(flag){break;}//找到相同的元素,common已经计算出
+//    }
+//    return common;
+//}
+//
+//void next(CHARSTR *strchild,int nextarr[]){
+//    int length = strchild->length;
+//    for (int i = 1; i<=length; i++) {
+//        nextarr[i] = each_next(strchild,i);
+//    }
+//}
+//
+//void input(CHARSTR *str){
+//    for(int j = 1;j<1001;j++){
+//        char letter('0');
+//        scanf("%c",&letter);
+//        if(letter == '\n'){
+//            str->length = (j-1);
+//            break;
+//        }
+//        str->arr[j] = letter;
+//    }
+//}
+//
+//int main(){
+//    int n(0);
+//    char enter;
+//    scanf("%d",&n);
+//    scanf("%c",&enter);
+//    int* result = new int[n];
+//    CHARSTR strchild;
+//    CHARSTR str;
+//    for (int i = 0; i<n; i++) {
+//        input(&strchild);
+//        input(&str);
+//        int nextarr[11]={0};//与字符串数据结构一致
+//        next(&strchild,nextarr);
+//        result[i] =match(&str,&strchild,nextarr);
+////        cout<<match(str,strchild,nextarr)<<endl;
+//    }
+//    
+//    for(int i = 0;i<n;i++){
+//        if(i == n-1){
+//            cout<<result[i];
+//        }else{
+//            cout<<result[i]<<endl;
+//        }
+//        
+//    }
+//    delete []result;
+//    return 0;
+//}
--- uoj/uoj/value-of-express.cpp
+++ uoj/uoj/value-of-express.cpp
@@ -1,9 +1,148 @@
-//
-//  value-of-express.cpp
-//  uoj
-//
-//  Created by admos on 16/9/19.
-//  Copyright © 2016年 admos. All rights reserved.
-//
+//#35
+//迭代器 next 保持状态，遇到括号等突变，先考虑最简单的情况 1+2 ,然后考虑括号，是否可以用递归解决
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+typedef enum type{
+    number=1,
+    symbol,
+    leftbracket,
+    rightbracket
+} TYPE;
+//状态
+typedef struct state{
+    TYPE type;
+    char colloct[65];
+    int pointer;
+} STATE;
+
+typedef struct nstack{
+    double stack[1000];
+    int pointer;
+} NSTACK;
+
+typedef struct sstack{
+    char stack[1000];
+    int pointer;
+} SSTACK;
+
+static STATE state;
+
+TYPE get_type(char letter){
+    TYPE type;
+    if(letter == '+'||letter == '-'||letter == '*'||letter == '/'||letter == '='){type = symbol;return type;}
+    if(letter == '('){type = leftbracket;return type;}
+    if(letter == ')'){type = rightbracket;return type;}
+    type = number;
+    return type;
+}
+//执行两个数计算的函数
+double execcompute(char sym,double noee,double noer){// = 不会进来，因为没有边缘
+    double result(0);
+    if (sym == '+') {return noee+noer;}
+    if (sym == '-') {return noee-noer;}
+    if (sym == '*') {return noee*noer;}
+    if (sym == '/') {return noee/noer;}
+    return result;
+}
+//
+double compute(NSTACK *nostac,SSTACK *symstack){
+    double result;
+    if(nostac->pointer == 1){//到栈底了
+        result =  nostac->stack[0];
+    }else{
+//        symstack->pointer-=1;
+//        symstack->stack[symstack->pointer];//符号出栈
+        //数字出栈两个
+        nostac->stack[nostac->pointer-2] = execcompute(symstack->stack[symstack->pointer-1],nostac->stack[nostac->pointer-2],nostac->stack[nostac->pointer-1]);
+        nostac->pointer -=1;
+        symstack->pointer -=1;
+        result = compute(nostac, symstack);//递归计算自己
+    }
+    return result;
+}
+
+//
+int dispatch_handle(TYPE type,NSTACK *nostack,SSTACK *symstack){//type是新字符的类型
+    switch (state.type) {
+        case number:
+            if(type == symbol){
+                state.colloct[state.pointer] = '\0';
+                nostack->stack[nostack->pointer] = atof(state.colloct);
+                nostack->pointer+=1;
+            }else if(type == rightbracket){
+                //遇到右括号 ，相当于回车
+            }
+            break;
+        case symbol:
+            if(type == number){
+                symstack->stack[symstack->pointer] = state.colloct[0];
+                symstack->pointer+=1;
+            }else if (type == leftbracket){
+                //遇到左括号，开始考虑递归，注意state
+            }
+            break;
+        case leftbracket:
+            ;
+            break;
+        case rightbracket:
+            ;
+            break;
+        default:
+            //state.type为0是初始状态，收集即可
+//            state.type = type;
+//            return -1;
+            break;
+    }
+    return 0;
+}
+//消费者
+int  consum(char letter,NSTACK *nostack,SSTACK *symstack){
+    TYPE type = get_type(letter);
+        if(type != state.type){//边缘触发
+//            if(dispatch_handle(type,nostack,symstack) == -1){break;}
+            dispatch_handle(type,nostack,symstack);
+            state.colloct[0] = letter;
+            state.pointer = 1;
+            state.type = type;
+            return 0;
+    }
+    state.colloct[state.pointer] = letter;
+    state.pointer +=1;
+    return 0;
+}
+//输入
+double input(){
+    char letter('0');
+    NSTACK number;
+    SSTACK sym;
+    number.pointer = 0;
+    sym.pointer = 0;
+    while (1) {
+        scanf("%c",&letter);
+        if(letter == '\n'){break;}
+        //中缀表达式变为后缀 然后再用栈计算
+        consum(letter,&number,&sym);
+    }
+    return compute(&number,&sym);
+}
+
+int main(){
+    
+    char enter;
+    int n;
+    scanf("%d%c",&n,&enter);
+    
+    //状态
+    for(int i = 0;i<n;i++){
+        printf("%.2f",input());
+    }
+    
+    return 0;
+}
