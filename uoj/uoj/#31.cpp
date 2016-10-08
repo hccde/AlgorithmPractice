83960864fc397874f73481ed282c246d17de6c79
hccde <1076663958@qq.com>
2016-10-08 14:33:37 +0800

    水题,#33结束回去做数据结构

---
+++ uoj/uoj/#31.cpp
@@ -0,0 +1,24 @@
+////
+////  #31.cpp
+////  uoj
+////
+////  Created by admos on 2016/10/8.
+////  Copyright © 2016年 admos. All rights reserved.
+////
+//
+//#include <stdio.h>
+//#include <iostream>
+//
+//int main(){
+//    int max(-1);
+//    int min(10001);
+//    int input;
+//    
+//    for(int i = 0;i < 5;i++){
+//        scanf("%d",&input);
+//        max = input>max?input:max;
+//        min = input<min?input:min;
+//    }
+//    printf("%d %d",min,max);
+//    return 0;
+//}
---
+++ uoj/uoj/#33.cpp
@@ -0,0 +1,19 @@
+//
+//  #33.cpp
+//  uoj
+//
+//  Created by admos on 2016/10/8.
+//  Copyright © 2016年 admos. All rights reserved.
+//蛇形填数
+
+#include <stdio.h>
+#include <iostream>
+
+using std::endl;
+using std::cin;
+using std::cout;
+
+int main(){
+    
+    return 0;
+}
