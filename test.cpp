b3a4eab78945b6ba3ba7f1be3632d905c0ebb55b
hccde <1076663958@qq.com>
2017-06-04 23:24:48 +0800

    test.cpp

---
+++ test.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <stdarg.h>
+#include <memory>
+
+using namespace std;
+
+class alloc{
+private:
+	typedef int* storage;
+	size_t _size;
+	size_t residual;
+	storage ram_poll;
+	
+public:
+	alloc(size_t size):_size(size),residual(size){
+		int _size = size % sizeof(int) == 0?size/sizeof(int):size/sizeof(int)+1;
+		ram_poll = new int[_size];
+	}
+	
+	~alloc(){
+		delete []ram_poll;
+	}
+};
+
+void error_handler(){
+	cout<<"tes";
+}
+
+int main(int argc , char** argv){
+	set_new_handler(error_handler);
+	alloc s =  alloc(40);
+	return 0;
+}
