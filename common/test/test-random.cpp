ae10d08f56c3f31aaa77429d5e5db1b0f78effb1
hccde <1076663958@qq.com>
2017-06-07 12:55:45 +0800

    random

--- common/test/test-random.cpp
+++ common/test/test-random.cpp
@@ -2,8 +2,12 @@
 #include "../lib/random.hpp"
 using namespace std;
 int main(int argc,char** argv){
-	produce(10);
+	cout<<random_number(10)<<endl;
+	cout<<random_number(12)<<endl;
+	cout<<random_number(12)<<endl;
+	cout<<random_number(12)<<endl;
+	// random_numbers(10,10);
 	return 0;
 }
 
-// g++11 test-random.cpp ../lib/random.cpp -o random
+// g++11 test-random.cpp ../lib/random.cpp -o rando
