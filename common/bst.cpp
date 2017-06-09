41faf58bb808bae2a1bf1bf003e656b23ab060e1
hccde <1076663958@qq.com>
2017-06-09 16:15:35 +0800

    vaild bst

--- common/bst.cpp
+++ common/bst.cpp
@@ -4,12 +4,13 @@
 using namespace std;
 
 int main(int argc,char** argv){
-    vector<int> randnums = random_numbers(20,20);
+    vector<int> randnums = random_numbers(100,20);
     // show_vector(randnums); 
-    cout<<endl;
-    int arr[]={19, 18 ,8 ,7 ,0 ,19, 1, 15, 9, 7, 11, 19, 4, 5, 9, 6, 6, 18, 5, 9};
-    vector<int> v(arr,arr+20);
-    Bst<int> b = Bst<int>(v);
-    // b.node_traversal(b.root);
+    Bst<int> b = Bst<int>(randnums);
+    // b.traversal(b.root);
+    bool is_vaild =  b.vaild_bst(b.root);
+    if(!is_vaild){
+        cout<<"erro tree";
+    }
     return 0;
 }
--- common/lib/bst.hpp
+++ common/lib/bst.hpp
@@ -19,50 +19,74 @@ class Bst{
 
         }
     };
+
+    typedef void* handler(Node*);
+    void default_handler(Node* node){
+        std::cout<<node->value<<" ";
+    }
+    void vaild_handler(Node* node){
+        std::cout<<"ha";
+    }
+
     Node* root;
     Bst(vector<T> nums){
         root = new Node(nums[0],NULL,NULL,NULL);
-        // show_vector(nums);
-        // std::cout<<std::endl;
-        build_bst(nums.begin()++,nums.end());
+        typename vector<T>::iterator begin = nums.begin()+1;
+        build_bst(begin,nums.end());
     }
 
     Node* find_node(T value,Node* node){
-        if((node->value > value && node->left == NULL) ||
-        (node->value <= value && node->right == NULL)){
+        if((node->value >= value && node->left == NULL) ||
+        (node->value < value && node->right == NULL)){
             return node;
         }else{
             Node* tem = NULL;
-            value >= node->value?tem = node->right:tem = node->left;
+            value > node->value?tem = node->right:tem = node->left;
             return find_node(value,tem);
         }
     }
 
     void insert_node(T value){
-        std::cout<<value<<std::endl;
         Node* parent = find_node(value,root);
         Node* node  = new Node(value,parent,NULL,NULL);
-        parent->value > value ? parent->left = node:parent->right = node;
+        parent->value >= value ? parent->left = node:parent->right = node;
     }
 
     void build_bst(typename vector<T>::iterator iter,typename vector<T>::iterator end){
-        // if(iter!=end){
-        //     insert_node(*iter);
-        //     typename vector<T>::iterator iters = iter+1;
-        //     build_bst(iters,end);
-        // }
-        for(;iter!=end;iter++){
-            // std::cout<<*iter<<" ";
+        if(iter!=end){
             insert_node(*iter);
+            typename vector<T>::iterator iters = iter+1;
+            build_bst(iters,end);
         }
     }
 
     void traversal(Node* node){
-        // if(node != NULL){
-        //     std::cout<<node->value<<" ";
-        //     traversal(node->left);
-        //     traversal(node->right);
-        // }
+        if(node != NULL){
+            std::cout<<node->value<<" ";
+            traversal(node->left);
+            traversal(node->right);
+        }
+    }
+
+    bool vaild_bst(Node* node){
+        bool res(true);
+        if(node != NULL){
+            if(node->left!=NULL){
+                if(node->left->value>node->value){
+                    return false;
+                }   
+            }
+            if(node->right!=NULL){
+                if(node->right->value<=node->value){
+                    return false;
+                }
+            }
+            res = vaild_bst(node->left);
+            if(!res){return false;}
+            res = vaild_bst(node->right);
+            if(!res){return false;}
+        }
+        return res;
     }
     ~Bst(){
         //todo
