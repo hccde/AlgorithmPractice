bb5ff33eb637548ac3e6af13c7f7499cffcb11dc
hccde <1076663958@qq.com>
2017-10-09 13:47:29 +0800

    1004

--- sjtoj/sjtoj/1004.cpp
+++ sjtoj/sjtoj/1004.cpp
@@ -1,34 +1,39 @@
-//#include <iostream>
-//#include <stdio.h>
-//using std::cin;
-//using std::cout;
-//using std::endl;
-//
-//typedef struct RoadInfo {
-//    int m = 0;
-//    int t = 0;
-//    int u = 0;
-//    int f = 0;
-//    int d = 0;
-//    
-//} RoadInfo;
-//
-//int main(){
-//    RoadInfo roadinfo;
-//    cin>>roadinfo.m>>roadinfo.t>>roadinfo.u>>roadinfo.f>>roadinfo.d;
-//    char segment ='u';
-//    
-//    int time= 0;
-//    int farest =0;
-//    bool switchButton = true;
-//    for (int i=1; i<=roadinfo.t; i++) {
-//        cin>>segment;
-//        if(!switchButton) {continue;}
-//        
-//        segment=='f'?time = time + roadinfo.f*2: time = time + roadinfo.u+roadinfo.d;
-//        time<roadinfo.m?farest = i:switchButton=false;
-//    }
-//    
-//    cout<<farest<<endl;
-//    return 0;
-//}
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <cstring>
+#include <string>
+#include <stdio.h>
+#include <algorithm>
+using namespace std;
+
+
+int main()
+{ 		
+	int m,t,u,f,d,sum(0),result(0);
+	char flag;
+	bool status(true);
+	cin>>m>>t>>u>>f>>d;
+	for(int i = 0;i<t;i++){
+		cin>>flag;
+		if(!status){
+			continue;
+		}
+
+		if(flag == 'u' || flag == 'd'){
+			sum = sum+(u+d);
+		}else{
+			sum = sum+2*f;
+		}
+
+		if(sum >= m){
+			result = i;
+			status = false;
+		}
+	}
+	if(result == 0){
+		result = t;
+	}
+	cout<<result;
+	return 0;	
+}
